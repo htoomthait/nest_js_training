ad221d0312ebfbfee58e64774b70de9089b48be1
Htoo Maung Thait <htoomaungthait@gmail.com>
2024-09-14 15:49:35 +0630

    logger tested

--- first_training_at_bp/src/app.controller.ts
+++ first_training_at_bp/src/app.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger, LoggerService } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  private readonly logger: LoggerService = new Logger(AppController.name);
+
   constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): any {
     const message = this.appService.getHello();
+
+    this.logger.log("This hello world function!");
+    this.logger.warn("This is waring message");
+    this.logger.debug(`This is debug message ${message}`);
+    this.logger.error(`This is sample error message!`);
+    this.logger.verbose(`This is sample verbose message!`)
 
     return {
       "status": "success",
--- first_training_at_bp/src/main.ts
+++ first_training_at_bp/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger("MAIN FILE");
+  const appEnv: string = "production";
+
+  logger.verbose(` APP ENV = ${appEnv}`)
+
+  Logger.overrideLogger(
+    appEnv == "development" ?
+      ['error', 'warn', 'debug', 'fatal', 'verbose', 'log'] :
+      ['error', 'warn']
+  );
+
+
   await app.listen(9000);
 }
 bootstrap();
